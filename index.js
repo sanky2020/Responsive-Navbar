ad48b796fe8e2387a5e425d18a5f58c24409db04
sankyk2020 <69984129+sanky2020@users.noreply.github.com>
2022-02-18 16:12:25 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let bar = document.getElementById("bar");
+let navbar= document.querySelector(".navbar");
+let nav = document.getElementById(".nav")
+
+bar.addEventListener("click", function(){
+    navbar.classList.toggle("fadeaway")
+})
